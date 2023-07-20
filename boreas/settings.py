c7ed68ec881bbbc98e65c90ac99b3e2ca3387aaf
Chris Pressland <mail@cpressland.io>
2023-07-20 18:16:47 +0100

    Switch to using RabbitMQ DSNs

--- boreas/settings.py
+++ boreas/settings.py
@@ -1,16 +1,9 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AmqpDsn
 
 
 class Settings(BaseSettings):
     keyvault_uri: str | None = None
-    rabbitmq_user: str = "guest"
-    rabbitmq_pass: str = "guest"
-    rabbitmq_host: str = "localhost"
-    rabbitmq_port: int = 5672
-
-    @property
-    def rabbitmq_dsn(self) -> str:
-        return f"amqp://{self.rabbitmq_user}:{self.rabbitmq_pass}@{self.rabbitmq_host}:{self.rabbitmq_port}//"
+    rabbitmq_dsn: AmqpDsn = "amqp://guest:guest@localhost:5672"
 
 
 settings = Settings()
