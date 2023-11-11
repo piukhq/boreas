3772e58e7535780f5e5b1e5558b2dcc358c07820
Chris Pressland <mail@cpressland.io>
2023-11-11 14:40:15 +0000

    Fix high latency by moving AMQP connections out of the HTTP loop

--- boreas/views.py
+++ boreas/views.py
@@ -14,6 +14,8 @@ router = APIRouter(
     dependencies=[Depends(get_api_key)],
 )
 
+conn = Connection(str(settings.rabbitmq_dsn), connect_timeout=3)
+
 
 async def invalid_token_exc_handler(request, exc: InvalidTokenError):
     return JSONResponse(status_code=exc.status_code, content=exc.content)
@@ -25,6 +27,5 @@ async def transactions(
     transactions: list[RetailTransaction],
 ) -> None:
     counter.labels(merchant_slug=retailer_id).inc()
-    with Connection(str(settings.rabbitmq_dsn), connect_timeout=3) as conn:
-        for transaction in transactions:
-            message_queue.add(transaction.model_dump(), retailer_id=retailer_id, connection=conn)
+    for transaction in transactions:
+        message_queue.add(transaction.model_dump(), retailer_id=retailer_id, connection=conn)
